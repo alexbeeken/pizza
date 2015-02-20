96bf55c5fdfde6f31eb9149536a960f1514f7341
Alex Beeken <aeb242@gmail.com>
2015-02-20 10:28:10 -0800

    correctly calculate slices and price

--- spec/specs.js
+++ spec/specs.js
@@ -3,3 +3,13 @@ describe('pizzaPrice', function() {
     expect(pizzaPrice(1, true)).to.equal("$1.25");
   });
 });
+
+describe('sliceCalculate', function() {
+  it("returns the number of slices in a 12'' pizza", function() {
+    expect(slicer(12)).to.equal("8");
+  });
+
+  it("concatenates an adjective to the end of the outut string if size is less than 3", function() {
+    expect(slicer(1))
+  })
+})
