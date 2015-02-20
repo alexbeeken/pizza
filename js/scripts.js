a94694e9f719d347485f8d7e59c8dc106e1efa1a
Alex Beeken <aeb242@gmail.com>
2015-02-20 10:57:16 -0800

    Store pizza as an object with data and methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,55 @@
-var pizzaPrice = function(size, hasPepperoni) {
+var Pizza = {
+  size: 0,
 
-  var price = 0.00;
+  hasPepperoni: false,
 
-  if (hasPepperoni){
-    price += 0.25 * size;
-  }
+  price: function() {
 
-  if (size < 10) {
-    price += size;
-  } else {
-    price += (size * 1.25);
-  }
+    var price = 0.00;
 
-  return ("$" + price.toFixed(2).toString());
-
-}
-
-var slicer = function(size) {
-  var area = (3.14 * Math.pow(size/2, 2));
-  rawOutput = (area/15);
-  var output = Math.ceil(rawOutput).toString();
-
-  console.log(rawOutput + " " + output + " " + area);
-
-  if (size < 3) {
-    if (rawOutput < 1) {
-      var end = " tiny slice";
-    } else {
-      var end = " tiny slices";
+    if (this.hasPepperoni){
+      price += 0.25 * this.size;
     }
-  } else if (output === '1'){
-    var end = " slice";
-  } else {
-    var end = " slices";
+
+    if (this.size < 10) {
+      price += this.size;
+    } else {
+      price += (this.size * 1.25);
+    }
+
+    return ("$" + price.toFixed(2).toString());
+  },
+
+  slices: function() {
+    var area = (3.14 * Math.pow(this.size/2, 2));
+    var rawOutput = (area/15);
+    var output = Math.ceil(rawOutput).toString();
+
+    if (this.size < 3) {
+      if (rawOutput < 1) {
+        var end = " tiny slice";
+      } else {
+        var end = " tiny slices";
+      }
+    } else if (output === '1'){
+      var end = " slice";
+    } else {
+      var end = " slices";
+    }
+
+    return (output + end);
   }
-
-  return (output + end);
-
 
 }
 
 $(document).ready(function() {
   $("#formOrder").submit(function(event) {
     event.preventDefault();
-    var size = parseInt($("#size").val());
-    var addPepperoni = ($("#addPepperoni")[0].checked);
-    $("#price").text(pizzaPrice(size, addPepperoni));
-    $("#slices").text(slicer(size));
+    var pizza = Object.create(Pizza);
+    pizza.size = parseInt($("#size").val());
+    pizza.hasPepperoni = ($("#addPepperoni")[0].checked);
+    $("#price").text(pizza.price);
+    $("#slices").text(pizza.slices);
     $("#priceShow").show();
   });
 })
--- spec/specs.js
+++ spec/specs.js
@@ -1,15 +1,30 @@
-describe('pizzaPrice', function() {
-  it("returns $1.25 for a 1 inch pizza", function() {
-    expect(pizzaPrice(1, true)).to.equal("$1.25");
+describe('Pizza', function() {
+  describe('price', function() {
+    it("returns $1.25 for a 1 inch pizza", function() {
+      var test_pizza = Object.create(Pizza);
+      test_pizza.size = 1;
+      test_pizza.hasPepperoni = true;
+      expect(test_pizza.price()).to.equal("$1.25");
+    });
+  });
+
+  describe('slices', function() {
+    it("returns the number of slices in a 12'' pizza", function() {
+      var test_pizza = Object.create(Pizza);
+      test_pizza.size = 12;
+      expect(test_pizza.slices()).to.equal("8 slices");
+    });
+
+    it("returns correct grammar for size, 1 and 2 slices", function() {
+      var test_pizza1 = Object.create(Pizza);
+      test_pizza1.size = 1;
+      var test_pizza2 = Object.create(Pizza);
+      test_pizza2.size = 5;
+      var test_pizza3 = Object.create(Pizza);
+      test_pizza3.size = 4;
+      expect(test_pizza1.slices()).to.equal("1 tiny slice")
+      expect(test_pizza2.slices()).to.equal("2 slices")
+      expect(test_pizza3.slices()).to.equal("1 slice")
+    })
   });
 });
-
-describe('sliceCalculate', function() {
-  it("returns the number of slices in a 12'' pizza", function() {
-    expect(slicer(12)).to.equal("8");
-  });
-
-  it("concatenates an adjective to the end of the outut string if size is less than 3", function() {
-    expect(slicer(1))
-  })
-})
